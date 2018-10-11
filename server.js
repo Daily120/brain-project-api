7b2a8746dad91406d50a0afc582cc1ef1740cc9c
Daily120 <forzhacker@mail.ru>
2018-10-11 18:13:59 +0200

    Deploying on heroku

---
+++ server.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+const Clarifai = require('clarifai');
+
+const clarifaiApp = new Clarifai.App({
+    apiKey: '48804f9b595f4e8b95c3bf5adac434b0'
+});
+
+const handleApiCall = (req, res) => {
+    clarifaiApp.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+        .then(data => {
+            res.json(data);
+        })
+        .catch(err => res.status(400).json('unable to work with API'))
+}
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: '123',
+        database: 'smart-brain'
+    }
+})
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors()); //for unsecure requests. just to test the app on a localhsot
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    db.select('email', 'hash').from('login').where('email', '=', req.body.email)
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if (isValid) {
+            return db.select('*').from('users').where('email', '=', req.body.email)
+            .then(user => {
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+            res.status(400).json('wrong credentials')
+        }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    if (!email || !name || !password) {
+        return res.status(400).json('incorrect form submission');
+    }
+    const hash = bcrypt.hashSync(password);
+
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
+        }).into('login').returning('email')
+        .then(loginEmail => {
+            return trx('users')
+                .returning('*')
+                .insert({
+                    email: loginEmail[0],
+                    name: name,
+                    joined: new Date()
+            })
+            .then(user => {
+                res.json(user[0]);
+            })
+        })
+        .then(trx.commit) //everything ok? apply chsnges
+        .catch(trx.rollback) //error? rollback changes
+    })
+    .catch(err => res.status(400).json('unable to register'))
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({id}) //ES6 syntax=== where({id: id})
+        .then(user => user[0] ? res.json(user[0]) : res.status(400).json('Not found'))
+        .catch(err => res.status(400).json('error getting user'))
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => res.json(entries[0]))
+        .catch(err => res.status(400).json('unable to get entries'))
+})
+
+app.post('/imageurl', (req, res) => { handleApiCall(req, res) })
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
+
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
